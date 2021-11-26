2254b14efe0c57efdc8124bf2688f4381fa23265
AnutaWest <ane4karpova2003@gmail.com>
2021-11-26 19:04:49 +0300

    Добавление макетов страниц

--- app/src/main/java/com/example/myapplication/InformationAboutTheSolution.java
+++ app/src/main/java/com/example/myapplication/InformationAboutTheSolution.java
@@ -1,6 +1,28 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import android.os.Bundle;
 
@@ -10,5 +32,9 @@ public class InformationAboutTheSolution extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_about_the_solution);
+    }
+    public void moveToDownload(View view) {
+        Intent intent = new Intent(this, SimilarSolutions.class);
+        startActivity(intent);
     }
 }
